c5e88ae9bf88f8bcd689b27d0f2df3623f8a6cb7
Ali Manson <fastest0111@proton.me>
2023-11-11 01:27:18 +0900

    Create Table Functionality Added

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -42,6 +42,25 @@ def sign_up(user: UserSignUp):
     # Implement your sign-up logic here
     # Create a new user with the provided details and return a response
     cursor = db.cursor()
+
+    # Check if the table exists
+    cursor.execute("SHOW TABLES LIKE 'users'")
+    table_exists = cursor.fetchone()
+
+    if not table_exists:
+        # Create the 'users' table if it doesn't exist
+        cursor.execute("""
+            CREATE TABLE users (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                level INT DEFAULT 0,
+                expire_day DATE
+            )
+        """)
+
+    # Insert the new user into the 'users' table
     query = "INSERT INTO users (username, email, password) VALUES (%s, %s, %s)"
     values = (user.username, user.email, user.password)
     cursor.execute(query, values)
