5f4f83ab414d8fb07b307729fa00b93ebab190e2
datngyenster <dat@gusher.co>
2023-11-15 05:39:17 +0900

    HTTPs Redirect

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from app.routers import auth
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 import stripe
+from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
+from ssl import SSLContext
 
 app = FastAPI()
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    HTTPSRedirectMiddleware
 )
+
+ssl_context = SSLContext()
+ssl_context.load_cert_chain("cert.pem", "key.pem")
 
 stripe.api_key = "sk_test_51OAYN0ItQ91j83DilxeRLixL8nBtOwbGiJ5KSlB65qG576Eans0deS8osZ5vknUd2rej0R3FfcIOjvXiKpwBFgre003XBuMXBQ"
 
@@ -23,5 +24,7 @@ if __name__ == '__main__':
         host="0.0.0.0",
         port=80,
         reload=True,
-        workers=1
+        workers=1,
+        ssl_keyfile="key.pem",
+        ssl_certfile="cert.pem"        
     )
